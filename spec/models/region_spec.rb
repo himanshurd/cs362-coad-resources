317bfde6e04b1e39b01712c944e9910c82d21bd5
gkansala13 <kansalag@oregonstate.edu>
2022-01-30 23:34:29 -0800

    added region unit test to_s
    #6

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
+
+  let (:region) { Region.new(name: 'Fake Region') }
   it 'exists' do
     Region.new
   end
 
-  
   describe "attributes" do
     it "has a name" do 
       region = Region.new
       expect(Region).to respond_to(:name)
+    end
+  end
+
+  describe "#to_s" do 
+    it "returns the name" do 
+      name = region.name
+      region = Region.new(name: name)
+      expect(region.to_s).to eq(name)
     end
   end
 
