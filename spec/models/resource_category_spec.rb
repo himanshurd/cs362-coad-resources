6d9b4f74f8a5e8ed2db6f1fcd991810753262ad2
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-02-07 11:56:07 -0800

    Did refactoring and create a unit test for methods
    References #19

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,16 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+
+  let(:resource_category) {ResourceCategory.new(name: 'Fake Resource Category')}
   it 'exists' do
     ResourceCategory.new
   end
 
   describe "attributes" do
     it "has a name" do
-      resource_category = ResourceCategory.new
       expect(resource_category).to respond_to(:name)
     end
   end
+
+  describe "#to_s" do
+    it 'returns the name' do
+      name = resource_category.name
+      expect(resource_category.to_s).to eq(name)
+    end
+  end
+
+  describe "::unspecified" do
+    it "returns the resourse category with the name 'unspecified' " do
+      resource_category = ResourceCategory.unspecified
+      expect(resource_category.name).to eq('unspecified')
+    end
+  end
+  
+
+
+
 
   
 
