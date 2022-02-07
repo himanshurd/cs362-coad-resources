4130fcb8784c1f4ce9f6cf198ae615eaaf13f5f5
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-02-07 14:14:13 -0800

    Create a test for validations References #21

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -29,14 +29,29 @@ RSpec.describe ResourceCategory, type: :model do
 
   describe "associations" do
     it "it has many tickets" do
-      expect(region).to have_many(:tickets)
+      expect(resource_category).to have_many(:tickets)
     end
   end
   
+  describe "validations" do
+    it "cannot have a black name" do
+      name = resource_category.name
+      expect(name).to be_valid
+      resource_category.name = nil
+      expect(resource_category).to_not be_valid
+    end
 
+    it "cannot have a length greater than 255" do
+      expect(resource_category).to validates_length_of(resource_category.name).is_at_least(1).is_at_most(255)
+    end
 
-
-
-  
-
+    it "must have a unique name" do
+      duplicate_name = 'Fake Resource Category'
+      resource_category = ResourceCategory.create!(name: duplicate_name)
+      duplicate_resource_category = ResourceCategory.new(nam: 'Unique Name')
+      expect(duplicate_resource_category).to be_valid
+      duplicate_resource_category.name = duplicate_name
+      expect(duplicate_resource_category).to be_invalid
+    end
+  end
 end
