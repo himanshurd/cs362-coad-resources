2ca05cdf5cec60f40d38d1bb51e42ffe9b2cc792
gkansala13 <kansalag@oregonstate.edu>
2022-02-06 21:12:20 -0800

    Organization: Factory created
    Reference #15

---
+++ spec/factories/organizations.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :organization do
+    name {"fake Name"}
+    id {"fake id"}
+    email {"Fake@Email.com"}
+    status {"approved"}
+    phone {"+15037998087"}
+    description {"fake organization"}
+    rejection_reason {"No reason"}
+    liability_insurance {"None"}
+    primary_name {"Fake Primary Name"}
+    secondary_name {"Fake Seondary Name"}
+    secondary_phone {"+15034567890"}
+    title {"Fake Org Title"}
+    transportation{"yes"}
+  end
+end
