d6bf39b10a7024d5e47d4ae7a05f585845b6df8b
gkansala13 <kansalag@oregonstate.edu>
2022-02-08 07:48:40 -0800

    Region: Added unique test, needs to be improved
    Reference #31

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -34,7 +34,11 @@ RSpec.describe Region, type: :model do
     end
 
     it "Name length 1-255" do 
-      expect(region).to  validate_length_of(:name).is_at_least(1).is_at_most(255)
+      should validate_length_of(:name).is_at_least(1).is_at_most(255)
+    end
+
+    it "Must be unique" do 
+      should validate_uniqueness_of(:name).case_insensitive
     end
   end
 end
