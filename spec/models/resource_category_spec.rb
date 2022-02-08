22e2f0993c9e9533d525c780c2695b79c27e2f63
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-02-07 19:26:25 -0800

    Fix all the logical and syntactical errors with matchers #28

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-  let (:resource_category) { build(:resource_category) }
-  
-  it "has a name" do 
-    expect(resource_category).to respond_to(:name)
-  end 
+  let(:resource_category) {ResourceCategory.new(name: "Fake Resource  Category") }
 
   describe "attributes" do
     it "has a name" do
@@ -14,44 +10,42 @@ RSpec.describe ResourceCategory, type: :model do
     end
   end
 
-  describe "#to_s" do
+  describe "validations" do 
+    it { resource_category.should validate_presence_of(:name) }
+    it { resource_category.should validate_length_of(:name).is_at_least(1).is_at_most(255) }
+    it { resource_category.should validate_uniqueness_of(:name).case_insensitive }
+  end
+
+
+  describe "methods" do
     it 'returns the name' do
       name = resource_category.name
       expect(resource_category.to_s).to eq(name)
     end
+
+    it "activate" do
+      resource_category.activate
+      expect(resource_category.active).to be_truthy
+    end
+
+    it "deactivate" do
+      resource_category.deactivate
+      expect(resource_category.activate).to be_truthy
+    end
+
+    it "inactivate?" do
+      expect(resource_category.inactive?).to be_falsy
+    end
+
+    it "returns the resourse category with the name 'unspecified' " do
+      resource_category = ResourceCategory.unspecified
+      expect(resource_category.name).to eq('Unspecified')  
+    end  
+  end
+
+  describe "associations" do
+    it { resource_category.should have_many(:tickets) }
+    it { resource_category.should have_and_belong_to_many(:organizations) }
   end
 end
-#   describe "::unspecified" do
-#     it "returns the resourse category with the name 'unspecified' " do
-#       resource_category = ResourceCategory.unspecified
-#       expect(resource_category.name).to eq('unspecified')
-#     end
-#   end
 
-  # describe "associations" do
-  #   it "it has many tickets" do
-  #     expect(resource_category).to have_many(:tickets)
-  #   end
-  # end
-  
-  # describe "validations" do
-  #   it "cannot have a black name" do
-  #     name = resource_category.name
-  #     expect(name).to be_valid
-  #     resource_category.name = nil
-  #     expect(resource_category).to_not be_valid
-  #   end
-  # end
-#   it "cannot have a length greater than 255" do
-#     expect(resource_category).to validates_length_of(resource_category.name).is_at_least(1).is_at_most(255)
-#   end
-
-#   it "must have a unique name" do
-#     duplicate_name = 'Fake Resource Category'
-#     resource_category = ResourceCategory.create!(name: duplicate_name)
-#     duplicate_resource_category = ResourceCategory.new(nam: 'Unique Name')
-#     expect(duplicate_resource_category).to be_valid
-#     duplicate_resource_category.name = duplicate_name
-#     expect(duplicate_resource_category).to be_invalid
-#   end
-# end
