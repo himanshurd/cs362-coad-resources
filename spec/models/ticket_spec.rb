4e25c2809829c6003ab63677b0e3a70b235103d2
gkansala13 <kansalag@oregonstate.edu>
2022-02-08 08:27:59 -0800

    Ticket: Created belongs to region attribute test
    Reference #33

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
     
-    let (:ticket) { build_stubbed(:ticket) }
+  let (:ticket) { build_stubbed(:ticket) }
 
-    describe "factory test" do
-        it "has a name" do
-          expect(ticket).to respond_to(:name)
-        end
+  describe "factory test" do
+    it "has a name" do
+      expect(ticket).to respond_to(:name)
     end
+  end
+
+  describe "Attributes" do 
+    it "Belongs to a Region" do 
+      should belong_to(:region)
+    end
+  end
 end
