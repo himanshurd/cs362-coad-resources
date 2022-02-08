2fbc81739a0f7e87321e475ebab7740b2670e9ea
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-02-07 16:15:55 -0800

    Intialize an organization class References #23

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -9,33 +9,36 @@ RSpec.describe Organization, type: :model do
         end
 
         it "has the email" do
-            expect(organization).to to_respond(:email)
+            expect(organization).to respond_to(:email)
         end
 
         it "has the phone" do
-            expect(organization).to to_respond(:phone)
+            expect(organization).to respond_to(:phone)
         end
 
         it "has the status" do
-            expect(organization).to to_respond(:status)
+            expect(organization).to respond_to(:status)
         end
 
         it "has the transportation" do
-            expect(organization).to to_respond(:transporation)
+            expect(organization).to respond_to(:transportation)
         end
 
         it "has the primary name" do
-            expect(organization).to to_respond(:primary_name)
+            expect(organization).to respond_to(:primary_name)
         end
 
         it "has the secondary name" do
-            expect(organization).to to_respond(:secondary_name)
+            expect(organization).to respond_to(:secondary_name)
         end
 
         it "has the secondary phone" do
-            expect(organization).to to_respond(:secondary_phone)
+            expect(organization).to respond_to(:secondary_phone)
         end
     end
+
+
+
 
     
 
