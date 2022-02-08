6fa22e64ef8621b54844b206f754a99153fc63a4
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-02-07 17:04:49 -0800

    Create a unit test for validations References #25

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-    let(:organization) {Organization.new(email: "test@test.com", name: "FakeOrganization", phone: "0000000000", status: "submitted", transportation: "yes", primary_name: "PrimaryFakeName", secondary_name: "SecondFakeOrganisation", secondary_phone: "1111111111")}
+    let(:organization) {Organization.new(email: "test@test.com", name: "FakeOrganization", phone: "0000000000", status: "submitted", transportation: "yes", primary_name: "PrimaryFakeName", secondary_name: "SecondFakeOrganisation", secondary_phone: "1111111111", description: "Fake description here")}
 
     describe "attributes" do
         it "has the name" do
@@ -35,11 +35,72 @@ RSpec.describe Organization, type: :model do
         it "has the secondary phone" do
             expect(organization).to respond_to(:secondary_phone)
         end
+
+        it "has the description" do
+            execute(description).to respond_to(:description)
+    end
+    
+    describe "validations" do
+        it "validate presence of name" do
+            should validates_presence_of(:name)
+        end
+
+        it "validate presence of email" do
+            should validates_presence_of(:email)
+        end
+
+        it "validate presence of phone" do
+            should validates_presence_of(:phone)
+        end
+
+        it "validate presence of status" do
+            should validates_presence_of(:status)
+        end
+
+        it "validate presence of primary name" do
+            should validates_presence_of(:primary_name)
+        end
+
+        it "validate presence of secondary name" do
+            should validates_presence_of(:secondary_name)
+        end
+
+        it "validate presence of secondary phone" do
+            should validates_presence_of(:secondary_phone)
+        end
+
+        it "the restricted length of the email" do
+            should validates_length_of(:email).is_at_least(1).is_at_most(255)
+        end
+
+        it "the restricted length of the name" do
+            should validates_length_of(:name).is_at_least(1).is_at_most(255)
+        end
+
+        it "valid format of the email" do
+            should allow_value(organization.email).for(:email)
+        end
+
+        it "check the uniqueness of email" do
+             should validate_uniqueness_of(:email).case_insensitive
+        end
+
+        it "check the uniqueness of name" do
+            should validate_uniqueness_of(:name).case_insensitive
+        end
+
+        it "check the length of name" do
+            should validate_length_of(:name).is_at_least(1).is_at_most(1020)
+        end
     end
 
 
-
-
+       
     
+    
+
+
+
+
 
 end
