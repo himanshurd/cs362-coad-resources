07e95c0baad5499fd17b933cc15a680fd24f2152
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-02-07 16:04:23 -0800

    create a unit test for attributes

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,12 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-    
-  let (:organization) { build_stubbed(:organization) }
+    let(:organization) {Organization.new(email: "test@test.com", name: "FakeOrganization", phone: "0000000000", status: "submitted", transportation: "yes", primary_name: "PrimaryFakeName", secondary_name: "SecondFakeOrganisation", secondary_phone: "1111111111")}
 
-  describe "attributes" do
-    it "has a name" do
-      expect(organization).to respond_to(:name)
+    describe "attributes" do
+        it "has the name" do
+            expect(organization).to respond_to(:name)
+        end
+
+        it "has the email" do
+            expect(organization).to to_respond(:email)
+        end
+
+        it "has the phone" do
+            expect(organization).to to_respond(:phone)
+        end
+
+        it "has the status" do
+            expect(organization).to to_respond(:status)
+        end
+
+        it "has the transportation" do
+            expect(organization).to to_respond(:transporation)
+        end
+
+        it "has the primary name" do
+            expect(organization).to to_respond(:primary_name)
+        end
+
+        it "has the secondary name" do
+            expect(organization).to to_respond(:secondary_name)
+        end
+
+        it "has the secondary phone" do
+            expect(organization).to to_respond(:secondary_phone)
+        end
     end
-  end
+
+    
+
 end
