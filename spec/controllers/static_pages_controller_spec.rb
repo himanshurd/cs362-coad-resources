aa34b3dcf5c52d0cb74362b77d67ecd43675115d
gkansala13 <kansalag@oregonstate.edu>
2022-03-09 23:56:26 -0800

    Static_page_controller: Added non user index test, no redirection
    Reference #58

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
+  describe "Non Logged in User" do
+    it "No Redirection" do
+      get :index
+      expect(response.redirect?).to be(false)
+    end
+  end
 end
