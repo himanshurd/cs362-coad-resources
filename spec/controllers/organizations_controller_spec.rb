bb85dd7732b20859729a079811a1a7cba31eea8a
gkansala13 <kansalag@oregonstate.edu>
2022-03-09 16:13:59 -0800

    Organization Controller: Added non logged in users
    Reference #50

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
-  
+  describe "Non logged in User" do 
+    it "redirects into sign in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      get :edit, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :show, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :update, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      put :update, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      post :approve, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      post :reject, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 end
