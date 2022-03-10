f47d720662303ee4630b8208b7cf622a38354285
gkansala13 <kansalag@oregonstate.edu>
2022-03-09 23:01:40 -0800

    Tickets Controller: Non logged in user test
    Reference #53

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  describe "Non Logged in User" do 
+    it "Redirects Back to Dashboard" do
+      get :show, params: {id: 'Fake'} 
+      expect(response).to redirect_to(dashboard_path)
+      post :capture, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
+      post :release, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
+      patch :close, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
+      delete :destroy, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
+end 
 
-end
