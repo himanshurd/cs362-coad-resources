cc54f641ee03b80238ddbd3824d47b13fb82de9d
gkansala13 <kansalag@oregonstate.edu>
2022-03-09 23:46:02 -0800

    Resource_category controller: Non logged-in user tests
    Refrence #56

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
-
+  describe "Non Logged in User" do 
+    it "Redirects Back to Sign-In Screen" do
+      patch :activate, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :deactivate, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      get :edit, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :show, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :update, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      put :update, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+      delete :destroy, params: {id: 'Fake'}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 end
