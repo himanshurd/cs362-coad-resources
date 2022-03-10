34537dd1ebc8d5c458c77f78f74211228b79183a
gkansala13 <kansalag@oregonstate.edu>
2022-03-10 00:02:21 -0800

    Dashboard_controller: Non Logged in user test
    Reference #59

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-
+  describe "Non Logged in User" do 
+    it "Redirects Back to Sign-In Screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 end
