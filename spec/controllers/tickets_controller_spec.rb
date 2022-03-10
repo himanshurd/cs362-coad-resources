e98843fa5c4cef3219ac87fb7464b15e9ed890e4
gkansala13 <kansalag@oregonstate.edu>
2022-03-09 23:20:54 -0800

    Ticket Controller: Added Unapproved Org User Tests
    Reference #55

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -16,12 +16,33 @@ RSpec.describe TicketsController, type: :controller do
     end
   end
 
-  describe "an approved Organization user" do 
-    it "redirects to dashboard"  do
+  describe "Approved Organization User" do 
+    it "Redirects Back to Dashboard"  do
       organization_user = create(:user)
       organization_user.confirm
       sign_in(organization_user)
       
+      delete :destroy, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
+
+  describe "Unapproved Organization User" do 
+    it "Redirects Back to Dashboard"  do
+      organization_user = create(:user)
+      organization_user.confirm
+      organization_user.organization=nil
+      organization_user.save
+      sign_in(organization_user)
+
+      get :show, params: {id:'Fake'}
+      expect(response).to redirect_to(dashboard_path)
+      post :capture, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
+      post :release, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
+      post :close, params: {id: 'Fake'}
+      expect(response).to redirect_to(dashboard_path)
       delete :destroy, params: {id: 'Fake'}
       expect(response).to redirect_to(dashboard_path)
     end
