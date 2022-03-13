34aee9b22ec1b353d2ad4ca7a3b009b8bde6248f
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-03-12 23:46:14 -0800

    User log in test for returned user Reference #65

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+    scenario "log in for returned user" do
+        user = create(:user)
+        user.confirm
+        visit 'login'
+    
+        fill_in "Email address", with: user.email
+        fill_in "Password", with: user.password
+    
+        click_on "Sign in"
+        expect(page).to have_text "Signed in successfully."
+      end
+      scenario "new user tries to log in" do
+        visit 'login'
+
+        fill_in "Email address", with: "Fake@Email.com"
+        fill_in "Password", with: "FAKE"
+    
+        click_on "Sign in"
+        expect(page).to have_text "Invalid Email or password."
+      end
+      scenario "Wrong password by returned user" do
+        user = create(:user)
+        user.confirm
+        visit 'login'
+    
+        fill_in "Email address", with: user.email
+        fill_in "Password", with: "Incorrect_Password"
+        click_on "Sign in"
+        expect(page).to have_text "Invalid Email or password."
+      end
 
 end
