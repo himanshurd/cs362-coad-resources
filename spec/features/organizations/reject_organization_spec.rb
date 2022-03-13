8d1c8bfdef2c9cd361ec354ec9a56025f812394d
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-03-13 09:41:28 -0700

    create a test for rejecting an organization Reference #68

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+    it "shows a result" do
+        admin = create(:user, :admin)
+        admin.confirm
+        log_in_as(admin)
     
-
+        visit "/organizations"
+        click_link("Pending")
+        click_on("Reject")
+        #expect(page).to have_text('not accepted')
+      end
 end
