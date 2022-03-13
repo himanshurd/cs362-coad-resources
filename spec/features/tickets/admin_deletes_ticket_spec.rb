e2ec3f986585b4979b1667eb5fbf07e2720b3972
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-03-13 10:02:38 -0700

    test for when admin deletes the ticket Reference #70

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
+    before do
+        create(:ticket, name: "Delete_ticket")
+        user = create(:user, :admin)
+        user.confirm
+        log_in_as(user)
+        visit '/dashboard'
+      end
+    
+      scenario "successful in deletion of ticket" do
+        click_on "Delete_ticket"
+        click_on "Delete"
+        expect(page).to_not have_text("Delete_ticket")
+      end
 
 end
