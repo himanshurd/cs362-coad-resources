1ce181f0dd77be300d25af3a84afa42603f3fee6
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-03-12 23:52:52 -0800

    Test for a new user Reference #66

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
-
+    scenario "New user" do
+        visit 'signup'
+    
+        fill_in "Email address", with: "Fake.new@Email.com"
+        fill_in "Password", with: "New_Fake_password"
+    end
 end
