1d065fa1fdf80ecdaaa043985ec3a9c3536e686a
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-03-13 09:51:47 -0700

    create a test for updating a user's information Reference #69

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
-
+    it "Update user's information" do
+        user = create(:user, :organization)
+        user.confirm
+        log_in_as(user)
+        user.organization.approve
+        user.organization.save!
+    
+        visit "/dashboard"
+        
+        click_on("Edit Organization")
+        click_on("Update")
+        expect(page).to have_current_path("/organizations/#{user.organization.id}")
+      end
 end
