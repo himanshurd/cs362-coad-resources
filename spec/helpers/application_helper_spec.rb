5f1de18925a20a62932e26a58bbf7fc34f85a750
gkansala13 <kansalag@oregonstate.edu>
2022-03-13 22:35:25 -0700

    Created application_helper tests
    Reference #77

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-
+  describe "full_title helper function" do
+    it "Displays base title if not not given a page title" do
+      expect(helper.full_title).to eq("Disaster Resource Network")
+    end
+    it "Displays base title and the given a page title" do
+      page_title = "Page Title"
+      expect(helper.full_title(page_title)).to eq("#{page_title} | Disaster Resource Network")
+    end
+  end
 end
