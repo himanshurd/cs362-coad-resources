c42fdc2af63f16b50aedadebf89393f9c17c7672
gkansala13 <kansalag@oregonstate.edu>
2022-03-13 21:31:38 -0700

    Created capture ticket system test
    Reference #74

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+  it "successful" do
+    user = create(:user)
+    user.confirm
+    log_in_as(user)
+    ticket = create(:ticket)
 
+    visit "/dashboard/#ticket"
+    expect(page).to have_text "#{ticket.name}"
+    click_on("#{ticket.name}")
+    click_on('Capture')
+    click_on("#{ticket.name}")
+    expect(page).to have_text "Release"
+    expect(page).to have_text "Close"
+  end
 end
