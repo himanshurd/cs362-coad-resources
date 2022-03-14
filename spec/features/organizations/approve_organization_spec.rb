8b5587eda8d9b5ffc6f4ddcddecfb490c77a05d4
gkansala13 <kansalag@oregonstate.edu>
2022-03-13 20:51:21 -0700

    Completed approve_organization
    Reference #73

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
-   
+  it "successful approval" do  
+    add_org=create(:organization, :set_submitted)
+    admin = create(:user, :admin_user)
+    admin.confirm
+    log_in_as(admin)
+    
+    visit "/organizations"
+    click_link("Pending")
+    click_on("Review")
+    click_on("Approve")
+    expect(page).to have_text('has been approved.')
+  end
 end
