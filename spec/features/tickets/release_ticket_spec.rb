07b557de324d01a7558b18e21313d78883af51b6
gkansala13 <kansalag@oregonstate.edu>
2022-03-13 21:46:59 -0700

    Created Release_ticket system test
    Reference #76

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
+  it "Succesful" do
+    user = create(:user)
+    user.confirm
+    log_in_as(user)
+    ticket = create(:ticket)
 
+    visit "/dashboard/#ticket"
+    expect(page).to have_text("#{ticket.name}")
+    click_on("#{ticket.name}")
+    click_on('Capture')
+    click_on("#{ticket.name}")
+    click_on('Release')
+    click_on("#{ticket.name}")
+    expect(page).to have_text("Capture")
+  end 
 end
