3e380c8916bb4db91283d72fa9ec1969de970575
gkansala13 <kansalag@oregonstate.edu>
2022-03-13 21:38:29 -0700

    Created Close_ticket system test
    Reference #75

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
+  it "Succesful" do
+    user = create(:user)
+    user.confirm
+    log_in_as(user)
+    ticket = create(:ticket)
 
+    visit "/dashboard/#ticket"
+    expect(page).to have_text("#{ticket.name}")
+    click_on("#{ticket.name}")
+    click_on('Capture')
+    click_on("#{ticket.name}")
+    click_on("Close")
+    click_on("#{ticket.name}")
+    expect(page).to have_text("Closed")
+    expect(page).to have_text("Release")
+  end 
 end
